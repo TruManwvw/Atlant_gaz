fee79230eeadececacea99e7f2d85b7e7bff6fe1
TruManwvw <kirilloff.vsevolod2017@yandex.ru>
2022-12-21 18:41:34 +0700

    'Commit_1'

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,30 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+//Добавление кнопке класса active и его элементу
+const tabsBtn   = document.querySelectorAll(".nav_link");
+const tabsItems = document.querySelectorAll(".content");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+   item.addEventListener("click", function() {
+      let currentBtn = item;
+      let tabId = currentBtn.getAttribute("data-tab");
+      let currentTab = document.querySelector(tabId);
+
+      if( ! currentBtn.classList.contains('_active') ) {
+         tabsBtn.forEach(function(item) {
+               item.classList.remove('_active');
+            });
+    
+            tabsItems.forEach(function(item) {
+               item.classList.remove('_active');
+            });
+
+          currentBtn.classList.add('_active');
+          currentTab.classList.add('_active');
+        }
+    });
+}
